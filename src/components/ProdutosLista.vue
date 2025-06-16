f09f17ba065ce0e4f9d0ac379e8f9c4b92fcc5cb
Cherlau Prado <cherlau_prado@hotmail.com>
2025-06-15 23:09:25 -0300

    add product list

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="produtos-container">
+    {{ produtos }}
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      produtos: [],
+    };
+  },
+  created() {
+    this.getProdutos();
+  },
+  methods: {
+    async getProdutos() {
+      const res = await fetch("http://localhost:3000/produto");
+      const json = await res.json();
+      this.produtos = json;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>Compre ou venda</h1>
+    <produtos-busca />
+    <produtos-lista />
+  </section>
+</template>
+
+<script>
+import ProdutosBusca from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    ProdutosBusca,
+    ProdutosLista,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
